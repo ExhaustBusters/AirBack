c8dce06a62c86a99aeb6e8d36a0f710cf512d059
Christian <chriamue@gmail.com>
2018-04-22 12:28:53 +0200

    added bootstrap

--- routes/api.js
+++ routes/api.js
@@ -18,40 +18,40 @@ const contract_interface = compiled.interface;
 const contract_bytecode = compiled.bytecode;
 
 /* GET users listing. */
-router.get('/data', function(req, res, next) {
-  res.send({sensorid:'superid'});
+router.get('/data', function (req, res, next) {
+    res.send({ sensorid: 'superid' });
 });
 
-var pollution = async function(res) {
+var pollution = async function (res) {
     await web3.eth.personal.unlockAccount(config.etherAccount, config.etherPassword);
     var contract = new web3.eth.Contract(JSON.parse(contract_interface), config.etherContract);
-    var result = await contract.methods.get_pollution().call(function(e, result){
-    console.log(result);
-    res.send({contract:config.etherContract, pollution:result});
-});
+    var result = await contract.methods.get_pollution().call(function (e, result) {
+        console.log(result);
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json({ contract: config.etherContract, pollution: result });
+    });
     console.log(result)
     await web3.eth.personal.lockAccount(config.etherAccount);
-    res.send({contract:config.etherContract, pollution:result});
 };
 
-router.get('/pollution', function(req, res, next) {
-  pollution(res);
+router.get('/pollution', function (req, res, next) {
+    pollution(res);
 });
 
-var addpollution = async function(res) {
+var addpollution = async function (res) {
     await web3.eth.personal.unlockAccount(config.etherAccount, config.etherPassword);
     var contract = new web3.eth.Contract(JSON.parse(contract_interface), config.etherContract);
     console.log(contract.methods.add_pollution);
-    var result = await contract.methods.add_pollution(7).send({from:config.etherAccount},function(e, result){
-    console.log(result);
-    res.send({contract:config.etherContract, transaction:result});
-});
+    var result = await contract.methods.add_pollution(7).send({ from: config.etherAccount }, function (e, result) {
+        console.log(result);
+        res.status(200).send({ contract: config.etherContract, transaction: result });
+    });
     console.log(result)
     await web3.eth.personal.lockAccount(config.etherAccount);
 };
 
-router.get('/addpollution', function(req, res, next) {
-  addpollution(res);
+router.get('/addpollution', function (req, res, next) {
+    addpollution(res);
 });
 
 module.exports = router;
